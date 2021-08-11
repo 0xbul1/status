f2ae7477c2a98a493d7e40a106d33839f7937282
赵旋 <xuanzhao@88.com>
2021-08-11 18:18:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <HeatMap width="500" type="bar" :options="chartOptions" :series="series" />
+</template>
+
+<script>
+import HeatMap from './components/HeatMap.vue';
+export default {
+  components: {
+    HeatMap,
+  },
+  data: function () {
+    return {
+      chartOptions: {
+        chart: {
+          id: 'vuechart-example',
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+        },
+      },
+      series: [
+        {
+          name: 'series-1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  border: 0;
+  margin: 0;
+  padding: 0;
+  outline: none;
+  list-style: outside none;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/HeatMap.vue
@@ -0,0 +1,12 @@
+<template>
+  <HeatMap />
+</template>
+<script>
+import VueApexCharts from 'vue3-apexcharts';
+
+export default {
+  components: {
+    HeatMap: VueApexCharts,
+  },
+};
+</script>
