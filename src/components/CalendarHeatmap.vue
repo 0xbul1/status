0fcfe7dde0fb1aca4f0e488bef6001cc2bff5765
赵旋 <xuanzhao@88.com>
2021-08-12 18:08:26 +0800

    fix HeatMap

--- src/components/CalendarHeatmap.vue
+++ src/components/CalendarHeatmap.vue
@@ -1,5 +1,13 @@
 <template>
-  <CalendarHeatmap :values="values" :end-date="endDate" />
+  <CalendarHeatmap
+    :values="values"
+    :end-date="endDate"
+    :range-color="['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39']"
+    :max="Infinity"
+    :min="-Infinity"
+    no-data-text=""
+    tooltip-unit="stars"
+  />
 </template>
 <script>
 import { CalendarHeatmap } from 'vue3-calendar-heatmap';
@@ -13,6 +21,13 @@ export default {
     },
     endDate: {
       default: +new Date(),
+    },
+  },
+  methods: {
+    test(a, b, c) {
+      console.log(a);
+      console.log(b);
+      console.log(c);
     },
   },
 };
